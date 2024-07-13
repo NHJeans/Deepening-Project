980b81610ef3d970d3f7f26e33b45ea91fe02043
NHJeans <guswo1013@naver.com>
2024-07-13 17:24:10 +0900

    remove: app폴더내 잘못 생성한 components폴더 삭제

--- src/app/components/Loading/ClubsLoadingSpinner.tsx
+++
@@ -1,9 +0,0 @@
-const LoadingSpinner = () => {
-  return (
-    <div className="flex justify-center items-center py-4">
-      <div className="animate-spin rounded-full h-8 w-8 border-t-4 border-b-4 border-customGreen"></div>
-    </div>
-  );
-};
-
-export default LoadingSpinner;
