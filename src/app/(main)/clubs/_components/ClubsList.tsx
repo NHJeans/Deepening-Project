ace14a648fbfcdadb5ba284a3b14dfbe56f5bc9b
NHJeans <guswo1013@naver.com>
2024-07-14 21:26:22 +0900

    fix: Image 태그내 변수명 변경

--- src/app/(main)/clubs/_components/ClubsList.tsx
+++ src/app/(main)/clubs/_components/ClubsList.tsx
@@ -33,13 +33,13 @@ const ClubsList = ({ myClubs }: { myClubs: boolean }) => {
     <div className="max-h-full overflow-y-auto p-4">
       <div className="grid grid-cols-2 gap-4">
         {filteredClubs.map((club: Club, index: number) => {
-          const imageUrl = club.thumbnail || "/Default-Card-Image.png";
+          const clubThumbnail = club.thumbnail || "/Default-Card-Image.png";
           const isLastClub = allClubs.length - 1 === index;
           return (
             <Link key={club.id} href={`/clubs/${club.id}/comments`}>
-              <div key={club.id} ref={isLastClub ? lastClubRef : null} className="bg-white p-4 rounded-lg shadow-md">
+              <div ref={isLastClub ? lastClubRef : null} className="bg-white p-4 rounded-lg shadow-md">
                 <div className="relative w-full h-32">
-                  <Image src={imageUrl} alt={club.title} layout="fill" objectFit="cover" className="rounded-lg" />
+                  <Image src={clubThumbnail} alt={club.title} layout="fill" objectFit="cover" className="rounded-lg" />
                 </div>
                 <h2 className="text-lg font-bold mt-2 truncate">{club.title}</h2>
               </div>
